1b93497481815842370f9258c95f93f2a18e7ab8
misabiko <misabiko@gmail.com>
2019-03-16 15:52:44 -0400

    Goal direction points cursor

--- Project/Core/World/Events - Jason/GoalDirection.gd
+++ Project/Core/World/Events - Jason/GoalDirection.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 func _process(delta):
-	$Sprite.rotate(delta)
+	pass
+	#$Sprite.rotate(delta)
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		$Sprite.rotation = $Sprite.position.angle_to_point(event.position)
+		#$ReferenceRect.rect_position = event.position
+		var rot = (event.position - $Sprite.global_position).angle() + PI/2
+		$Label.text = str(rot)
+		$Sprite.rotation = rot
