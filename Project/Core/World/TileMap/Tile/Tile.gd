11a21049375aa0e172d8800dcc0d6dc07283ecb1
oliperraul <olivier.perrault@usherbrooke.ca>
2019-03-15 20:33:02 -0400

    asdasda

--- Project/Core/World/TileMap/Tile/Tile.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-export(NodePath) onready var static_body
-
-
-func _ready():
-	static_body = get_node(static_body)
-	static_body.set_collision_layer_bit(CollisionLayers.SOLIDS, true)
-	static_body.set_collision_layer_bit(CollisionLayers.TILES, true)
