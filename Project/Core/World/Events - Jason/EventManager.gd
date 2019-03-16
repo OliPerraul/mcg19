6946870a19b5e801343fed03d41591b5b9469b5f
misabiko <misabiko@gmail.com>
2019-03-15 23:18:25 -0400

    Added camera focus

--- Project/Core/World/Events - Jason/EventManager.gd
+++ Project/Core/World/Events - Jason/EventManager.gd
@@ -17,10 +17,28 @@ func unhide_player():
 	emit_signal("unlock_controls")
 	$PLayer.visible = true
 
+func camera_cinematic(target_pos, travel_duration, focused_duration):
+	emit_signal("lock_controls")
+	var cam = $PLayer/Camera2D
+	var cam_pos = $PLayer/Camera2D.position
+	$CamTween.interpolate_property(cam, "position", cam_pos, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$CamTween.connect("tween_completed", self, "camera_cinematic_focus", [focused_duration, travel_duration, cam_pos])
+	$CamTween.start()
+
+func camera_cinematic_focus(object, key, focused_duration, travel_duration, old_position):
+	$CamTimer.wait_time = focused_duration
+	$CamTimer.connect("timeout", self, "camera_cinematic_return", [travel_duration, old_position])
+	$CamTimer.start()
+
+func camera_cinematic_return(travel_duration, target_pos):
+	$CamTween.interpolate_property($PLayer/Camera2D, "position", $PLayer/Camera2D.position, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$CamTween.connect("tween_completed", self, "emit_signal", ["unlock_controls"])
+	$CanvasLayer/BlackBars.deactivate_bars()
+	$CamTween.start()
+
 func _on_Dummy_area_entered(area):
 	interactable = area
 	print("entered: ", area.name)
-
 
 func _on_Dummy_area_exited(area):
 	interactable = null
@@ -28,6 +46,7 @@ func _on_Dummy_area_exited(area):
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
-		$BlackBars.deactivate_bars() if $BlackBars.activated else $BlackBars.activate_bars()
+		$CanvasLayer/BlackBars.deactivate_bars() if $CanvasLayer/BlackBars.activated else $CanvasLayer/BlackBars.activate_bars()
+		camera_cinematic($Truck.position, 1, 3)
 		if interactable:
 			unhide_player() if controlsLocked else hide_player()
