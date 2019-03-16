b6686e49a55df3e43e0e5de256593b4115e591bf
misabiko <misabiko@gmail.com>
2019-03-16 15:00:49 -0400

    Added TrashDial,TrashCan and GoalDirection

--- Project/Core/World/Events - Jason/GoalDirection.gd
+++ Project/Core/World/Events - Jason/GoalDirection.gd
@@ -1,13 +1,8 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _process(delta):
+	$Sprite.rotate(delta)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if event is InputEventMouseMotion:
+		$Sprite.rotation = $Sprite.position.angle_to_point(event.position)
