f15537162cf8156b26a05d8d45c285ce4a12ca87
misabiko <misabiko@gmail.com>
2019-03-16 19:47:55 -0400

    goal direction update

--- Project/Core/World/Events - Jason/GoalDirection.gd
+++ Project/Core/World/Events - Jason/GoalDirection.gd
@@ -1,14 +1,14 @@
 extends Node2D
 
-export (NodePath) var goal
-export var margin_h = 50
-var margin_v = 40
+export (float) var margin_h = 50
+export (float) var margin_v = 40
+var goal
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		var rect_extent = get_viewport().size / 2
 		
-		var to_goal = get_node(goal).position - event.position
+		var to_goal = goal.position - event.position
 		
 		to_goal = vec_min(
 			to_goal * ((rect_extent.x - margin_h) / max(abs(to_goal.x), 0.1)),
@@ -16,8 +16,11 @@ func _input(event):
 		)
 		
 		position = event.position + to_goal
-		var rot = (get_node(goal).position - $Sprite.global_position).angle() + PI/2
+		var rot = (goal.position - $Sprite.global_position).angle() + PI/2
 		$Sprite.rotation = rot
 
 func vec_min(a : Vector2, b : Vector2):
 	return a if a.length_squared() < b.length_squared() else b
+
+func set_goal(goal):
+	self.goal = goal
