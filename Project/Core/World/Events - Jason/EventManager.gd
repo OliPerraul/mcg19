62762019d067c421a0d245616cfe2d9b4a2015ab
misabiko <misabiko@gmail.com>
2019-03-15 21:28:27 -0400

    Added controls lock/unlock signals + Dummy player scene

--- Project/Core/World/Events - Jason/EventManager.gd
+++ Project/Core/World/Events - Jason/EventManager.gd
@@ -2,3 +2,31 @@ extends Node
 
 var controlsLocked = false
 
+var interactable
+
+signal lock_controls
+signal unlock_controls
+
+func hide_player():
+	controlsLocked = true
+	emit_signal("lock_controls")
+	$Dummy.visible = false
+
+func unhide_player():
+	controlsLocked = false
+	emit_signal("unlock_controls")
+	$Dummy.visible = true
+
+func _on_Dummy_area_entered(area):
+	interactable = area
+	print("entered: ", area.name)
+
+
+func _on_Dummy_area_exited(area):
+	interactable = null
+	print("exited: ", area.name)
+
+func _input(event):
+	if event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
+		if interactable:
+			unhide_player() if controlsLocked else hide_player()
