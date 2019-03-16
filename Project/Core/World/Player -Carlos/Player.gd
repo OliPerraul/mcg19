7c53339de56105d803330a4dd6134b3256a7c9ae
oliperraul <olivier.perrault@usherbrooke.ca>
2019-03-16 15:56:46 -0400

    asddas

--- Project/Core/World/Player -Carlos/Player.gd
+++ Project/Core/World/Player -Carlos/Player.gd
@@ -65,38 +65,6 @@ func _process(dt):
 		footprints.add(facing)
 		_last_foot_pos = global_position
 	
-
-func _physics_process(dt):
-
-	input_handler()
-
-	match(character_state): #str comparison lol				
-		"locked":
-			pass
-			#_player_set_locked()
-		"danger":
-			pass
-			#_player_danger()
-		"involuntary":
-			pass
-			#_player_involuntary()
-		"hidden":
-			_player_hidden()
-					
-		"enable_hide":    #DISPLAY UI HERE
-			_player_enable_hide()
-			continue #also do normal movement
-
-		"normal":
-			_player_normal()	
-
-			
-			#_player_involuntary()
-
-			
-
-
-
 func input_handler():
 
 	movement = Vector2(0,0)
@@ -131,39 +99,27 @@ func input_handler():
 	movement = movement.normalized()
 
 
-
-func _player_lock():
-	character_state = "locked"
-
-func _player_unlock():
-	character_state = "normal"
-
-func _player_set_visible():
-	$sprite.visible = true
-
-func _player_set_invisible():
-	$sprite.visible = false
-
-
 #states
-func _player_normal():
+func _player_normal_update():
 	move_and_slide(movement*250)
 	pass
 
-func _player_danger():
+func _player_danger_update():
 	#no movement
 	pass
 
-func _player_enable_hide():
+func _player_enable_hide_update():
 	if Input.is_action_just_released("ui_accept"):
-		character_state = "hidden"
+		init_state("hidden")
 		
 	if cover != null and not cover.area.overlaps_body(self): # goes outside
-		character_state = "normal"		
+		init_state("normal")		
 		cover = null
+	
+	
 
 
-func _player_hidden():
+func _player_hidden_update():
 	if cover != null :	
 		global_position = lerp(global_position, cover.global_position, hide_speed)
 		if Vectors.close_enough(global_position, cover.global_position):
@@ -171,19 +127,86 @@ func _player_hidden():
 		priority = -1
 		
 	if Input.is_action_just_released("ui_accept"):
-		character_state = "normal"
-		priority = 100
-		cover.z_index = 0
-	
+		init_state("normal")
+
+
+func _physics_process(dt):
+	update_state(character_state)
+
+func update_state(state):
+	match(state):
+		"locked":
+			pass
+			#_player_set_locked()
+		"danger":
+			pass
+			#_player_danger()
+		"involuntary":
+			pass
+			#_player_involuntary()
+		"hidden":
+			_player_hidden_update()
+					
+		"enable_hide":    #DISPLAY UI HERE
+			input_handler()
+			_player_normal_update()	
+			_player_enable_hide_update()
+
+		"normal":
+			input_handler()
+			_player_normal_update()	
+
+
+
+func init_state(state, args=[]):
+	if state == character_state:
+		return 
+		
+	character_state = state					
+					
+	match(state):
+		"locked":
+			pass
+			#_player_set_locked()
+		"danger":
+			pass
+			#_player_danger()
+		"involuntary":
+			pass
+			#_player_involuntary()
+		"hidden":
+			pass
+						
+		"enable_hide":    #DISPLAY UI HERE
+			self.cover = args[0]
+			
+			#continue #also do normal movement
+		"normal":
+			priority = 100
+			cover.z_index = 0
+			self.cover = null
+			pass
+
 
 func player_hide(cover):
-	if character_state != "hidden":
-		self.cover = cover
-		character_state = "enable_hide"
+	init_state('enable_hide', [cover])
 
+func _player_lock():
+	init_state('locked')
+
+func _player_unlock():
+	init_state('normal')
 
 func objective():
 	pass
+	
+
+
+func _player_set_visible():
+	$sprite.visible = true
+
+func _player_set_invisible():
+	$sprite.visible = false
 
 	
 	
