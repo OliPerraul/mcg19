a6a0853f6d6bf900a3de08856cfca83e9fe3be05
misabiko <misabiko@gmail.com>
2019-03-16 00:08:15 -0400

    Cleaned up EventManager code

--- Project/Core/World/Events - Jason/EventManager.gd
+++ Project/Core/World/Events - Jason/EventManager.gd
@@ -3,26 +3,26 @@ extends Node
 var controlsLocked = false
 var interactable
 var score = 0 setget update_score
+export (NodePath) var camera
 
 signal lock_controls
 signal unlock_controls
 
+signal hide_player
+signal unhide_player
+
 func hide_player():
 	controlsLocked = true
-	emit_signal("lock_controls")
-	$PLayer.visible = false
+	emit_signal("hide_player")
 
 func unhide_player():
 	controlsLocked = false
-	emit_signal("unlock_controls")
-	$PLayer.visible = true
+	emit_signal("unhide_player")
 
 func camera_cinematic(target_pos, travel_duration, focused_duration):
 	emit_signal("lock_controls")
-	var cam = $PLayer/Camera2D
-	var cam_pos = $PLayer/Camera2D.position
-	$CamTween.interpolate_property(cam, "position", cam_pos, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
-	$CamTween.connect("tween_completed", self, "camera_cinematic_focus", [focused_duration, travel_duration, cam_pos])
+	$CamTween.interpolate_property(camera, "position", camera.position, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$CamTween.connect("tween_completed", self, "camera_cinematic_focus", [focused_duration, travel_duration, camera.position])
 	$CamTween.start()
 
 func camera_cinematic_focus(object, key, focused_duration, travel_duration, old_position):
@@ -31,29 +31,23 @@ func camera_cinematic_focus(object, key, focused_duration, travel_duration, old_
 	$CamTimer.start()
 
 func camera_cinematic_return(travel_duration, target_pos):
-	$CamTween.interpolate_property($PLayer/Camera2D, "position", $PLayer/Camera2D.position, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$CamTween.interpolate_property(camera, "position", camera.position, target_pos, travel_duration, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$CamTween.connect("tween_completed", self, "emit_signal", ["unlock_controls"])
 	$CanvasLayer/BlackBars.deactivate_bars()
 	$CamTween.start()
 
 func _on_Dummy_area_entered(area):
 	interactable = area
-	print("entered: ", area.name)
 
 func _on_Dummy_area_exited(area):
 	interactable = null
-	print("exited: ", area.name)
 
 func update_score(new_score):
 	score = new_score
-	print("boop")
+	
 	$CanvasLayer/ScoreLabel.text = "Score: " + str(score)
 
 func _input(event):
 	if event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
-		$CanvasLayer/BlackBars.deactivate_bars() if $CanvasLayer/BlackBars.activated else $CanvasLayer/BlackBars.activate_bars()
-		camera_cinematic($Truck.position, 1, 3)
-		update_score(score + max(score, 1))
-		print(score)
 		if interactable:
 			unhide_player() if controlsLocked else hide_player()
