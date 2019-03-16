2604af1820e6ade50623db09a1d1a61d21290dbc
oliperraul <olivier.perrault@usherbrooke.ca>
2019-03-16 16:43:09 -0400

    asdasd

--- Project/Core/Start/Gameover.gd
+++ Project/Core/Start/Gameover.gd
@@ -6,5 +6,5 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _input(event):
-	if event.is_action_pressed('clieck'):
+	if event.is_action_pressed('click') || event.is_action_pressed('ui_accept'):		
 		get_tree().change_scene("res://Core/Start/StartScreen.tscn")
