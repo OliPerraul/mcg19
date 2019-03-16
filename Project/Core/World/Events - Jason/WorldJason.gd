93559bbe8465c8cd003c27c511bbc15d7b59d745
misabiko <misabiko@gmail.com>
2019-03-16 01:01:37 -0400

    Added Area2D to dummy player, and properly setup to interact with prop

--- Project/Core/World/Events - Jason/WorldJason.gd
+++ Project/Core/World/Events - Jason/WorldJason.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 func _input(event):
-	if event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
-		if $EventManager/CanvasLayer/BlackBars.activated:
-			$EventManager/CanvasLayer/BlackBars.deactivate_bars()
-		else:
-			$EventManager/CanvasLayer/BlackBars.activate_bars()
-	elif event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		$EventManager.score += max($EventManager.score, 1)
+
+func _on_EventManager_hide_player():
+	$Player._on_EventManager_lock_controls()
+	$EventManager/CanvasLayer/BlackBars.activate_bars()
+	$Player/Sprite.visible = false
+
+
+func _on_EventManager_unhide_player():
+	$Player._on_EventManager_unlock_controls()
+	$EventManager/CanvasLayer/BlackBars.deactivate_bars()
+	$Player/Sprite.visible = true
