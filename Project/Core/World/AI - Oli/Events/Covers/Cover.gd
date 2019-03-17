b028842d46e4a9b20ec5846102d052f2ca3ab33b
oliperraul <olivier.perrault@usherbrooke.ca>
2019-03-16 20:50:05 -0400

    Added trash

--- Project/Core/World/AI - Oli/Events/Covers/Cover.gd
+++ Project/Core/World/AI - Oli/Events/Covers/Cover.gd
@@ -1,13 +1,5 @@
 extends OLI_EVENT
 
-# OVERRIDE
-func handle(player):
-	if player.is_in_group("detectable"):
-		player.init_state('enable_hide', [self])
-		#player.remove_from_group("detectable")
-				
-		t_scale = Vector2.ONE*max_scale
-
 # ABSTR
 func _on_body_entered(b):
 	if b.is_in_group("detectable"):
--- Project/Core/World/AI - Oli/Events/Objectives/Objective.gd
+++ Project/Core/World/AI - Oli/Events/Objectives/Objective.gd
@@ -1,8 +1,5 @@
 extends OLI_EVENT
 
-
-
-
 # OVERRIDE
 func handle(player):
 	if player.is_in_group("detectable"):
