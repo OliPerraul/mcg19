f9a630f01b6a3f3e535f717dc6a0be3a4f3dd2b6
Archund <jarchund@gmail.com>
2019-03-17 09:21:09 -0400

    updated end scene

--- Project/Core/Start/Gameover.gd
+++ Project/Core/Start/Gameover.gd
@@ -4,6 +4,9 @@ extends Node
 # var a = 2
 # var b = "text"
 
+func _ready():
+	$Score.set_text("Score: "+String(Globals.game.score))
+
 # Called when the node enters the scene tree for the first time.
 func _input(event):
 	if event.is_action_pressed('click') || event.is_action_pressed('ui_accept'):		
