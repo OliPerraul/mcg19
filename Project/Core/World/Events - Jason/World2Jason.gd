f39b70fceff3f32dc9f4a08b42b59894695c5aee
misabiko <misabiko@gmail.com>
2019-03-17 06:42:07 -0400

    Fixed camera cinematic

--- Project/Core/World/Events - Jason/World2Jason.gd
+++ Project/Core/World/Events - Jason/World2Jason.gd
@@ -3,4 +3,4 @@ extends Node2D
 export (NodePath) var goal
 
 func _on_StartTimer_timeout():
-	$EventManager.camera_cinematic(get_node(goal).position, 1, 2.5, true)
+	$EventManager.camera_cinematic(get_node(goal).global_position, 1, 2.5, true)
